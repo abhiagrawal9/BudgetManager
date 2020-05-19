a6ce8b02e9c1e3b5b9d097558d916ff4b536f116
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2020-05-19 14:29:16 +0530

    added architecture

---
+++ app.js
@@ -0,0 +1,13 @@
+let bugdetController = (function () {
+   
+})();
+
+
+let UIController = (function () {
+    
+})();
+
+
+let appController = (function (bugdetCont,UICont) {
+    
+})(bugdetController,UIController);
